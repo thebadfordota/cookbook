9f0484d1ed40f71aebd8df003895cc7ffac713e7
Vasliy Babaykin <severmen18@gmail.com>
2021-07-07 22:43:49 +0400

    commit

--- main/forms.py
+++
@@ -1,9 +0,0 @@
-from .models import Recipe
-from django.forms import ModelForm, TextInput, NumberInput, DateInput, TimeInput, EmailInput
-
-
-class RecipeForm(ModelForm):
-
-    class Meta:
-        model = Recipe
-        fields = ['meal_type']
